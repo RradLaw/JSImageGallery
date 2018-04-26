6aebffe5eaa365208cb1dcc293bf1f54446402b6
Jarrad Law <jarrad.law@gmail.com>
2018-04-26 23:56:02 +0930

    Initial commit. Existing gallery CSS taken from debraannecreations site.

---
+++ images.js
@@ -0,0 +1,26 @@
+let imageJSON = {
+    "compName": "aaa",
+    "code": "18XD",
+    "sessions":
+        [
+            {
+                "title": "Session 1",
+                "subtitle": "Level 3",
+                "subfolder":
+                    [
+                        {
+                            "title": "club1",
+                            "images": [
+                                "1.jpg",
+                                "2.jpg"
+                            ]
+                        }
+                    ]
+                /*"images": [
+                    "1.jpg",
+                    "2.jpg"
+                ]*/
+            }
+        ],
+    "imageLocation": "images/"
+};
