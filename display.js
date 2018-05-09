f4dcd285078e1252350d374ebbae085fe3b91156
Jarrad Law <jarrad.law@gmail.com>
2018-05-09 20:07:21 +0930

    Fixed gallery display issues.

--- display.js
+++ display.js
@@ -8,7 +8,7 @@ function init() {
     clearGrid();
     document.getElementById('titleSpan').innerHTML = imageJSON.compName;
     for (let i = 0; i < imageJSON.subfolder.length; i++) {
-        document.getElementById('imgGrid').innerHTML += `<div class="pics" onclick="addDepth(${i});"><h2>${imageJSON.subfolder[i].title}</h2>${imageJSON.subfolder[i].subtitle ? imageJSON.subfolder[i].subtitle : ''}</div>`;
+        document.getElementById('imgGrid').innerHTML += `<div class="pics img" onclick="addDepth(${i});"><h2>${imageJSON.subfolder[i].title}</h2>${imageJSON.subfolder[i].subtitle ? imageJSON.subfolder[i].subtitle : ''}</div>`;
     }
     document.getElementById('breadcrumbs').innerHTML = '<span onclick="diveDepth(-1)">Home > </span>';
 }
@@ -23,7 +23,7 @@ function addDepth(d) {
     clearGrid();
     if (tag.subfolder) {
         for (let i = 0; i < tag.subfolder.length; i++) {
-            document.getElementById('imgGrid').innerHTML += `<div class="pics" onclick="addDepth(${i});"><h2>${tag.subfolder[i].title}</h2>${tag.subfolder[i].subtitle}</div>`;
+            document.getElementById('imgGrid').innerHTML += `<div class="pics img" onclick="addDepth(${i});"><h2>${tag.subfolder[i].title}</h2>${tag.subfolder[i].subtitle}</div>`;
         }
     } else {
         for (let i = 0; i < tag.images.length; i++) {
@@ -63,6 +63,7 @@ function openImage(img) {
 
     if (img < 0) img = 0;
     if (img > tag.images.length - 1) img = tag.images.length - 1;
+
     document.getElementById('imgGrid').innerHTML = `<center><img class="largeImage" src="${imageJSON.webDir}${tag.location}/large/${tag.images[img]}${imageJSON.imgExt}"><br>${tag.images[img]}<br><span class="link" onclick="openImage(--imgPointer)">Previous</span>&nbsp;<span class="link" onclick="openImage(++imgPointer)">Next</span><br><span class="link" onclick="diveDepth(depthPointer)">Back</span></center>`;
     imgPointer = img;
 }
